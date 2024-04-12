4b386a86efb15f91287a81da4beaef1fee6f1aac
LucasHerrero <lucasherrero2010@hotmail.com>
2024-04-12 20:15:56 +0200

    Fix variable naming and add console logs in auth component and service

--- src/app/auth/Auth.service.ts
+++ src/app/auth/Auth.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage-angular';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-constructor(private http: HttpClient, private Storage: Storage) { }
+constructor(private http: HttpClient, storage : Storage) { }
 
 login(email: string, password: string) {
   return this.http.post(`${environment.apiUrl}/login`, { email, password });
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,13 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent {
-  emailInic = "";
- PassInic = "";
+  emailInic :string = "";
+ PassInic : string = "";
   isLoginView = false; //TODO: CAMBIAR A TRUE PARA QUE APAREZCA EL LOGIN
 
 
   formInfoLogin(){
+console.log("Email: ", this.emailInic, "Password: ", this.PassInic);
     console.log(this.emailInic, this.PassInic);
+
   }
 
 
