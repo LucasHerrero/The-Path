e3f211bf4363a4e4df37a55dcefd405f7a63f7ff
LucasHerrero <lucasherrero2010@hotmail.com>
2024-05-05 16:27:45 +0200

    Update profile component HTML to display user's username

--- src/app/auth/profile/profile.component.ts
+++ src/app/auth/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../Auth.service';
 import { Router } from '@angular/router';
+import { IntJwtPayload } from '../IntJwtPayload';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +9,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  User: IntJwtPayload = {} as IntJwtPayload;
   constructor(private authService: AuthService, private route: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authService.isAuthenticated().then((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.authService.decodeToken().then((decodedToken: IntJwtPayload) => {
+          this.User = decodedToken;
+        });
+      }
+    });
+  }
 
   closeSession() {
     this.authService.logout();
