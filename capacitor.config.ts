19d7edd256ec1f5b6907a0a1dd98ad311bae831b
LucasHerrero <lucasherrero2010@hotmail.com>
2024-05-13 14:37:50 +0200

    Splash Name The Path

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'The-Path',
+  appName: 'The Path',
   webDir: 'www',
   server: {
     androidScheme: 'https'
